9db7ab6c466d7b988fce4a81496e55cfbf301935
nbrown <nbrown1@gmail.com>
2023-01-27 14:00:58 -0500

    Auto part add

--- docs/assets/section_images/build_label.sh
+++ docs/assets/section_images/build_label.sh
@@ -3,23 +3,32 @@
 OSCDIR=$1
 args="-Dfn=100 --render --colorscheme Parapart --autocenter --viewall --imgsize=512,512"
 
-if [ "$2" .eq "" ]
-then
-    echo "NO SECOND ARG"
-fi
-
-for t in home containers brackets fasteners standoffs tools components gears wheels structural hooks nuts bolts miscellaneous electrical hinges boxes trays holders 8020 wedges blocks motion washers screws fans motors displays switches potentiometers connectors pin_barrel four_sides more_sides mechanical motion linear_slides bushings couplers bar_stock search_results
-do
-    scadfile="scad/$t.scad"
+# Process a single file using scad if it exists or generating a label image if not
+process_file () {
+    label=`echo $1 | tr 'a-z' 'A-Z'`
+    scadfile="scad/$1.scad"
     if [ -f $scadfile ]
     then
         echo "HAVE SCAD FOR $label"
-        #$1/openscad $args $scadfile -o temp.png
-        #convert temp.png -transparent "#cc00cc" -geometry 128x128 $t.png
-        #rm temp.png
+        $1/openscad $args $scadfile -o temp.png
+        convert temp.png -transparent "#cc00cc" -geometry 128x128 $1.png
+        rm temp.png
     else
-        label=`echo $t | tr 'a-z' 'A-Z'`
         echo "NO SCAD FOR $label"
-        #convert -size 128x128 -background grey -fill black -gravity center label:"$label" "$t".png
+        convert -size 128x128 -background grey -fill black -gravity center label:"$label" "$1".png
     fi
+}
+
+# Just do one if it was passed in on the cmdline
+if [ ! -z "$2" ]
+then
+    echo "SECOND ARG"
+    process_file $2
+    exit 0
+fi
+
+# Generate the whole works
+for t in 8020 bar_stock blocks bolts boxes brackets bushings components connectors containers couplers displays electrical fans fasteners four_sides gears hinges holders home hooks linear_slides mechanical miscellaneous more_sides motion motion motors nuts pin_barrel potentiometers screws search_results standoffs structural switches tools trays washers wedges wheels
+do
+    process_file $t
 done
